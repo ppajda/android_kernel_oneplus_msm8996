de5a29f6de4038c86976656eff6059bc00076d8f
Chet Kener <Cl3Kener@gmail.com>
2014-10-07 22:20:29 -0400

    Add Arch Power and Gentle Fair Sleepers Interface
    * Credits to neobuddy and his chaos kernel which was discontinued.
    
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>
    
    Conflicts:
    	kernel/ksysfs.c
    	kernel/sched/fair.c

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -4797,10 +4797,17 @@ static void check_spread(struct cfs_rq *cfs_rq, struct sched_entity *se)
 #endif
 }
 
-static unsigned int Lgentle_fair_sleepers = 1;
+static unsigned int Lgentle_fair_sleepers = 0;
+static unsigned int Larch_power = 0;
+
 void relay_gfs(unsigned int gfs)
 {
 	Lgentle_fair_sleepers = gfs;
+}
+
+void relay_ap(unsigned int ap)
+{
+	Larch_power = ap;
 }
 
 static void
@@ -8072,6 +8079,7 @@ static void update_cpu_capacity(struct sched_domain *sd, int cpu)
 	unsigned long capacity = SCHED_CAPACITY_SCALE;
 	struct sched_group *sdg = sd->groups;
 
+
 	if (sched_feat(ARCH_CAPACITY))
 		capacity *= arch_scale_cpu_capacity(sd, cpu);
 	else
@@ -8080,6 +8088,7 @@ static void update_cpu_capacity(struct sched_domain *sd, int cpu)
 	capacity >>= SCHED_CAPACITY_SHIFT;
 
 	sdg->sgc->capacity_orig = capacity;
+
 
 	if (sched_feat(ARCH_CAPACITY))
 		capacity *= arch_scale_freq_capacity(sd, cpu);

9a17ed1e664cba71881251aa0f930b66c9f6e61a
Jerry Zhang <zhangjerry@google.com>
2017-09-27 11:49:44 -0700

    ANDROID: usb: gadget: f_mtp: Return error if count is negative
    If the user passes in a negative file size in a int64,
    this will compare to be smaller than buffer length,
    and it will get truncated to form a read length that
    is larger than the buffer length.
    
    To fix, return -EINVAL if the count argument is negative,
    so the loop will never happen.
    
    Bug: 37429972
    Test: Test with PoC
    Change-Id: I5d52e38e6fbe2c17eb8c493f9eb81df6cfd780a4
    Signed-off-by: Jerry Zhang <zhangjerry@google.com>
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- drivers/usb/gadget/function/f_mtp.c
+++ drivers/usb/gadget/function/f_mtp.c
@@ -886,6 +886,11 @@ static void send_file_work(struct work_struct *data)
 	offset = dev->xfer_file_offset;
 	count = dev->xfer_file_length;
 
+	if (count < 0) {
+		dev->xfer_result = -EINVAL;
+		return;
+	}
+
 	DBG(cdev, "send_file_work(%lld %lld)\n", offset, count);
 
 	if (dev->xfer_file_length >= 10 * 1024 * 1024) {
@@ -1016,6 +1021,11 @@ static void receive_file_work(struct work_struct *data)
 	filp = dev->xfer_file;
 	offset = dev->xfer_file_offset;
 	count = dev->xfer_file_length;
+
+	if (count < 0) {
+		dev->xfer_result = -EINVAL;
+		return;
+	}
 
 	DBG(cdev, "receive_file_work(%lld)\n", count);
 	if (!IS_ALIGNED(count, dev->ep_out->maxpacket))

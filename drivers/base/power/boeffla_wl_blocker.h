3ed8f0197717c0258d1c30c413dcc58cd490acbb
Christian S <crian84@gmail.com>
2018-06-08 16:54:53 +0200

    boeffla_wl_blocker: don't block wakelocks by default
    Signed-off-by: crian <crian84@gmail.com>

--- drivers/base/power/boeffla_wl_blocker.h
+++ drivers/base/power/boeffla_wl_blocker.h
@@ -14,10 +14,10 @@
  *
  */
 
-#define BOEFFLA_WL_BLOCKER_VERSION	"1.1.0"
+#define BOEFFLA_WL_BLOCKER_VERSION  "1.1.0"
 
-#define LIST_WL_DEFAULT				"qcom_rx_wakelock;wlan;wlan_wow_wl;wlan_extscan_wl;netmgr_wl;NETLINK"
+#define LIST_WL_DEFAULT             ""
 
-#define LENGTH_LIST_WL				255
-#define LENGTH_LIST_WL_DEFAULT		100
-#define LENGTH_LIST_WL_SEARCH		LENGTH_LIST_WL + LENGTH_LIST_WL_DEFAULT + 5
+#define LENGTH_LIST_WL              255
+#define LENGTH_LIST_WL_DEFAULT      123
+#define LENGTH_LIST_WL_SEARCH       LENGTH_LIST_WL + LENGTH_LIST_WL_DEFAULT + 5
